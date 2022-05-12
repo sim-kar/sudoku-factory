229973158cc72791aa03add73dd2948855233767
sika2001 <sika2001@student.miun.se>
2022-05-12 18:46:04 +0200

    Added documentation

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,20 +1,39 @@
 import java.util.Objects;
 
+/**
+ * An x, y position, such as a position on a game board. X is the column, y is the row.
+ */
 public class Position {
     private final int x;
     private final int y;
     private final int hashCode;
 
+    /**
+     * Create a new position at the column x and row y.
+     *
+     * @param x the column.
+     * @param y the row.
+     */
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
         this.hashCode = Objects.hash(x, y);
     }
 
+    /**
+     * Get the x value (column) of this position.
+     *
+     * @return the position's x value (column).
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * Get the y value (row) of this position.
+     *
+     * @return the position's y value (row).
+     */
     public int getY() {
         return this.y;
     }
