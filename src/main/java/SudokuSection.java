ef6446c1c4bb6095f1f7bc5c2b5c5e3d7802925a
Rasmus Djupedal <radj2000@student.miun.se>
2022-05-14 15:57:47 +0200

    - Section interface created - SudokuSection class created (empty)

---
+++ src/main/java/SudokuSection.java
@@ -0,0 +1,6 @@
+/**
+ * A Sudoku section represents a row, a column, or a 3x3 group of tiles in the game board.
+ * All its tiles have unique (correct) numbers.
+ */
+public class SudokuSection implements Section{
+}
