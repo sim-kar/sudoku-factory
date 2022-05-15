f6bd92ae212642cac30f8115bfe273eeb49c0a0b
Rasmus Djupedal <radj2000@student.miun.se>
2022-05-15 21:38:43 +0200

    - SudokuSection constructor implemented - SudokuSection skeleton written - Tile interface added method getPosition

--- src/main/java/SudokuSection.java
+++ src/main/java/SudokuSection.java
@@ -1,6 +1,42 @@
+import java.util.Map;
+import java.util.Set;
+
 /**
  * A Sudoku section represents a row, a column, or a 3x3 group of tiles in the game board.
  * All its tiles have unique (correct) numbers.
  */
 public class SudokuSection implements Section{
+
+    private Map<Position, Tile> tiles;
+
+    /**
+     * Adding all the passed Tiles to a Map, with everyone's Position as its key.
+     *
+     * @param tiles The tiles to add.
+     */
+    public SudokuSection(Set<Tile> tiles) {
+        tiles.forEach(tile -> {
+            this.tiles.put(tile.getPosition(), tile);
+        });
+    }
+
+    @Override
+    public Tile getTile(Position xy) {
+        return null;
+    }
+
+    @Override
+    public Set<Tile> getTiles() {
+        return null;
+    }
+
+    @Override
+    public boolean isCorrect() {
+        return false;
+    }
+
+    @Override
+    public Set<Tile> getIncorrectTiles() {
+        return null;
+    }
 }
