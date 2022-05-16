05e7160a01461d70600811e4914b48eea670487a
sika2001 <sika2001@student.miun.se>
2022-05-16 19:03:47 +0200

    Added tests for generating a solution

---
+++ src/test/java/SudokuSolverTest.java
@@ -0,0 +1,224 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class SudokuSolverTest {
+    Solver solver;
+    int[][] empty;
+
+    @BeforeEach
+    void setup() {
+        Random random = new Random(0L);
+        solver = new SudokuSolver(random);
+        empty = new int[9][9];
+        for (int[] row : empty) {
+            Arrays.fill(row, 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Generating a solution with null input throws an error")
+    void nullInputToGenerateSolutionThrowsError() {
+        assertThrows(IllegalArgumentException.class, () -> solver.generate(null));
+    }
+
+    @Test
+    @DisplayName("Generating a solution from input with numbers >9 throws error")
+    void numbersGreaterThanNineInInputToGenerateSolutionThrowsError() {
+        empty[1][1] = 10;
+        assertThrows(IllegalArgumentException.class, () -> solver.generate(empty));
+    }
+
+    @Test
+    @DisplayName("Generating a solution from input with numbers <0 throws error")
+    void numbersLessThanZeroInInputToGenerateSolutionThrowsError() {
+        empty[1][1] = -1;
+        assertThrows(IllegalArgumentException.class, () -> solver.generate(empty));
+    }
+
+    @Nested
+    @DisplayName("Sudoku Rules")
+    class SudokuRulesTest {
+        int[][] solution = solver.generate(empty);
+
+        @Test
+        @DisplayName("Generated solution only contains numbers 1-9")
+        void solutionOnlyHasNumbersOneToNine() {
+            Set<Integer> numbers = new HashSet<>();
+            for (int[] row : solution) {
+                for (int value : row) {
+                    numbers.add(value);
+                }
+            }
+
+            assertAll(
+                    () -> assertTrue(Collections.max(numbers) <= 9),
+                    () -> assertTrue(Collections.min(numbers) >= 1)
+            );
+        }
+
+
+        @Test
+        @DisplayName("Rows in generated solution have no duplicates")
+        void rowsInSolutionHasNoDuplicates() {
+            assertAll(
+                    () -> assertEquals(9, Arrays.stream(solution[0])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9,Arrays.stream(solution[1])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[2])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[3])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[4])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[5])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[6])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[7])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, Arrays.stream(solution[8])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size())
+            );
+        }
+
+        @Test
+        @DisplayName("Columns in generated solution have no duplicates")
+        void columnsInSolutionHasNoDuplicates() {
+            assertAll(
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][0])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][1])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][2])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][3])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][4])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][5])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][6])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][7])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size()),
+                    () -> assertEquals(9, IntStream.range(0, 9)
+                            .map(row -> solution[row][8])
+                            .boxed()
+                            .collect(Collectors.toSet())
+                            .size())
+            );
+        }
+
+        @Test
+        @DisplayName("Blocks in generated solution have no duplicates")
+        void blocksInSolutionHasNoDuplicates() {
+            Map<Position, Set<Integer>> blocks = Map.of(
+                    new Position(0, 0), new HashSet<>(),
+                    new Position(0, 1), new HashSet<>(),
+                    new Position(0, 2), new HashSet<>(),
+                    new Position(1, 0), new HashSet<>(),
+                    new Position(1, 1), new HashSet<>(),
+                    new Position(1, 2), new HashSet<>(),
+                    new Position(2, 0), new HashSet<>(),
+                    new Position(2, 1), new HashSet<>(),
+                    new Position(2, 2), new HashSet<>()
+            );
+            for (int row = 0; row < 9; row++) {
+                for (int column = 0; column < 9; column++) {
+                    // dividing the row and column index by 3 (floor division) gets the index
+                    // of the correct block
+                    Position xy = new Position(row / 3, column / 3);
+
+                    blocks.get(xy).add(solution[row][column]);
+                }
+            }
+
+            assertAll(
+                    () -> assertEquals(9, blocks.get(new Position(0, 0)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(0, 1)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(0, 2)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(1, 0)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(1, 1)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(1, 2)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(2, 0)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(2, 1)).size()),
+                    () -> assertEquals(9, blocks.get(new Position(2, 2)).size())
+            );
+        }
+    }
+
+
+    /**
+     * Generating a solution can be used to generate a new Sudoku board from an empty board, but
+     * only if the generated solution is random. If the solution is always the same only the same
+     * board will be generated!
+     *
+     * This test is to make sure that two generated solutions are not the same, by using a Random
+     * with different seed to generate two solutions and comparing them.
+     */
+    @Test
+    @DisplayName("Numbers in generated solution are randomized")
+    void solutionIsRandomized() {
+        int[][] solution = solver.generate(empty);
+
+        // generate another solution from a Random with a different seed
+        Random random2 = new Random(1L);
+        Solver solver2 = new SudokuSolver(random2);
+        int[][] solution2 = solver2.generate(empty);
+
+        assertNotEquals(solution2, solution);
+    }
+}
