4e9b1d9d1908cf99c3e62bfdb7b02612a0fea4e9
sika2001 <sika2001@student.miun.se>
2022-05-16 19:03:19 +0200

    Added Random as parameter to SudokuSolver

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+
 public class SudokuSolver implements Solver {
+    public SudokuSolver(Random random) {}
+
     @Override
     public int[][] generate(int[][] board) {
         return null;
