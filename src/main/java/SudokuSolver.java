4823ae531620a385df4fcd88227b5a72be5eb590
sika2001 <sika2001@student.miun.se>
2022-05-16 14:27:42 +0200

    Added SudokuSolver

---
+++ src/main/java/SudokuSolver.java
@@ -0,0 +1,11 @@
+public class SudokuSolver implements Solver {
+    @Override
+    public int[][] generate(int[][] board) {
+        return null;
+    }
+
+    @Override
+    public boolean isUnique(int[][] board) {
+        return false;
+    }
+}
