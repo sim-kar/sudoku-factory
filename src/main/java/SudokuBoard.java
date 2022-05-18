bcb4f834cde380bbc4801cfc77346761bc9d3e90
Rasmus Djupedal <radj2000@student.miun.se>
2022-05-18 15:01:48 +0200

    - Javadoc added

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -8,6 +8,13 @@ public class SudokuBoard implements Board{
     Map<Position, Section> columns = new HashMap<>();
     Map<Position, Section> blocks = new HashMap<>();
 
+    /**
+     * The SudokuBoard contains 9x9 Tiles and 27 Sections (one for each row, column, and 9 3x3 blocks
+     *
+     * @param rows The rows as an array of Sections
+     * @param columns The columns as an array of Sections
+     * @param blocks The blocks as an array of Sections
+     */
     public SudokuBoard(Section[] rows, Section[] columns, Section[] blocks) {
 
         // Populate the rows Map
