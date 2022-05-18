cfa0f1d208a9335e1de23097b4e59484c4730648
Rasmus Djupedal <radj2000@student.miun.se>
2022-05-18 14:31:57 +0200

    - Tests rewritten and cleaned up

--- src/test/java/SudokuBoardTest.java
+++ src/test/java/SudokuBoardTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,42 +9,54 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SudokuBoardTest {
+    SudokuSection[] rows = new SudokuSection[1];
+    SudokuSection[] columns = new SudokuSection[1];
+    SudokuSection[] blocks = new SudokuSection[1];
+    SudokuSection section_row2, section_col2;
+    SudokuBoard board;
 
-    @Test
-    @DisplayName("Create a board with a row and get the section for the row")
-    void createBoardReturnsRowSection() {
+    @BeforeEach
+    @DisplayName("Initialize variables for testing")
+    void initVars() {
+        Set<Tile> tiles;
 
-        Set<Tile> tiles = new HashSet<>();
+        //Initialize a row
+        tiles = new HashSet<>();
         tiles.add(new SudokuTile(3,new Position(2,4)));
         tiles.add(new SudokuTile(1,new Position(2,0)));
         tiles.add(new SudokuTile(7,new Position(2,8)));
 
-        SudokuSection section_row2 = new SudokuSection(tiles);
-        SudokuSection[] rows;
+        section_row2 = new SudokuSection(tiles);
         rows = new SudokuSection[1];
         rows[0] = section_row2;
-        SudokuBoard board = new SudokuBoard(rows, null, null);
 
+        // Initialize a column
+        tiles = new HashSet<>();
+        tiles.add(new SudokuTile(3,new Position(0,2)));
+        tiles.add(new SudokuTile(1,new Position(4,2)));
+        tiles.add(new SudokuTile(7,new Position(7,2)));
+
+        section_col2 = new SudokuSection(tiles);
+        columns = new SudokuSection[1];
+        columns[0] = section_col2;
+
+        // Initialize a block
+
+
+
+        board = new SudokuBoard(rows, columns, blocks);
+    }
+
+    @Test
+    @DisplayName("Create a board with a row and get the section for the row")
+    void createBoardReturnsRowSection() {
         Assertions.assertEquals(section_row2, board.getRow(2));
         Assertions.assertEquals(section_row2, board.getRow(new Position(2,0)));
-
     }
 
     @Test
     @DisplayName("Create a board with a column and get the section for the column")
     void createBoardReturnsColumnSection() {
-
-        Set<Tile> tiles = new HashSet<>();
-        tiles.add(new SudokuTile(3,new Position(0,2)));
-        tiles.add(new SudokuTile(1,new Position(4,2)));
-        tiles.add(new SudokuTile(7,new Position(7,2)));
-
-        SudokuSection section_col2 = new SudokuSection(tiles);
-        SudokuSection[] columns;
-        columns = new SudokuSection[1];
-        columns[0] = section_col2;
-        SudokuBoard board = new SudokuBoard(null, columns, null);
-
         Assertions.assertEquals(section_col2, board.getColumn(2));
         Assertions.assertEquals(section_col2, board.getColumn(new Position(0,2)));
     }
