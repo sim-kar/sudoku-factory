8e040ef3d8edd81ede8ad3554e17a596d14279b3
sika2001 <sika2001@student.miun.se>
2022-05-23 13:15:44 +0200

    Created interface BoardChangeObserver

---
+++ src/main/java/gui/BoardChangeObserver.java
@@ -0,0 +1,11 @@
+package gui;
+
+/**
+ * Observes changes to a puzzle game board.
+ */
+public interface BoardChangeObserver {
+    /**
+     * Can be used by the observed component to notify the observer that the board has been updated.
+     */
+    void updateBoard();
+}
