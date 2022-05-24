039ad93f06796ccc9e24b711465d4342ea666426
sika2001 <sika2001@student.miun.se>
2022-05-24 15:39:29 +0200

    Implemented SudokuController#createPuzzle

--- src/main/java/gui/SudokuController.java
+++ src/main/java/gui/SudokuController.java
@@ -3,6 +3,11 @@ package gui;
 import sudoku.Position;
 
 public class SudokuController implements Controller {
+    private final static int VERY_EASY_CLUES = 33;
+    private final static int EASY_CLUES = 31;
+    private final static int MEDIUM_CLUES = 29;
+    private final static int HARD_CLUES = 27;
+    private final static int VERY_HARD_CLUES = 25;
     private final Model model;
 
     public SudokuController(Model model) {
@@ -11,7 +16,13 @@ public class SudokuController implements Controller {
 
     @Override
     public void createPuzzle(Difficulty difficulty) {
-
+        switch (difficulty) {
+            case VERY_EASY -> model.createPuzzle(VERY_EASY_CLUES);
+            case EASY -> model.createPuzzle(EASY_CLUES);
+            case MEDIUM -> model.createPuzzle(MEDIUM_CLUES);
+            case HARD -> model.createPuzzle(HARD_CLUES);
+            case VERY_HARD -> model.createPuzzle(VERY_HARD_CLUES);
+        }
     }
 
     @Override
