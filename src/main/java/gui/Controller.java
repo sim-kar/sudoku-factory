19b53ceacbe215f85473a52de2e1a2ae9ddaeabd
sika2001 <sika2001@student.miun.se>
2022-05-24 14:46:13 +0200

    Created Difficulty enum

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -2,7 +2,11 @@ package gui;
 
 import sudoku.Position;
 
+enum Difficulty {
+    VERY_EASY, EASY, MEDIUM, HARD, VERY_HARD
+}
+
 public interface Controller {
-    void createPuzzle();
+    void createPuzzle(Difficulty difficulty);
     void setValueAt(Position xy, int value);
 }
