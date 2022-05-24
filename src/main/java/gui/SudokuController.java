3463b20843317e6a92dfff50a57f077334186370
sika2001 <sika2001@student.miun.se>
2022-05-24 14:47:04 +0200

    Created SudokuController

---
+++ src/main/java/gui/SudokuController.java
@@ -0,0 +1,15 @@
+package gui;
+
+import sudoku.Position;
+
+public class SudokuController implements Controller {
+    @Override
+    public void createPuzzle(Difficulty difficulty) {
+
+    }
+
+    @Override
+    public void setValueAt(Position xy, int value) {
+
+    }
+}
