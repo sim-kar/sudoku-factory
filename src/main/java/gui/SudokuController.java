1a2fb000ced71f5f3748795e7b7f4fd7567a256d
sika2001 <sika2001@student.miun.se>
2022-05-24 17:52:52 +0200

    Added javadoc to SudokuController

--- src/main/java/gui/SudokuController.java
+++ src/main/java/gui/SudokuController.java
@@ -2,6 +2,11 @@ package gui;
 
 import sudoku.Position;
 
+/**
+ * Can be used to update the state of a {@link Model}. Provides the strategy of how to update the
+ * model so that the caller doesn't have to concern itself with the details. Should be used with
+ * a {@link SudokuModel}.
+ */
 public class SudokuController implements Controller {
     private final static int VERY_EASY_CLUES = 33;
     private final static int EASY_CLUES = 31;
@@ -10,6 +15,11 @@ public class SudokuController implements Controller {
     private final static int VERY_HARD_CLUES = 25;
     private final Model model;
 
+    /**
+     * Create a new SudokuController for the given model.
+     *
+     * @param model the model that this controller will update
+     */
     public SudokuController(Model model) {
         this.model = model;
     }
