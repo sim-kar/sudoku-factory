8b8da27d584a3c82744b7b32b4e3b53ebde330f4
sika2001 <sika2001@student.miun.se>
2022-05-25 14:58:13 +0200

    Created SudokuView

---
+++ src/main/java/gui/SudokuView.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class SudokuView extends JFrame implements BoardChangeObserver, BoardSolvedObserver {
+    @Override
+    public void updateBoard() {
+
+    }
+
+    @Override
+    public void solved() {
+
+    }
+}
