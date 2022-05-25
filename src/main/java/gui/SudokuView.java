8d16f9027c6255eeeda8f8afdb55f5d849179b3d
sika2001 <sika2001@student.miun.se>
2022-05-25 14:59:53 +0200

    Added constructor to SudokuView

--- src/main/java/gui/SudokuView.java
+++ src/main/java/gui/SudokuView.java
@@ -3,6 +3,14 @@ package gui;
 import javax.swing.JFrame;
 
 public class SudokuView extends JFrame implements BoardChangeObserver, BoardSolvedObserver {
+    private final Model model;
+    private final Controller controller;
+
+    public SudokuView(Model model, Controller controller) {
+        this.model = model;
+        this.controller = controller;
+    }
+
     @Override
     public void updateBoard() {
 
